ca9de449a41d82f1904599da9266331ea446fd88
DaniilSh <dankshum@gmail.com>
2025-02-12 13:12:21 +0100

    implemented multithread server

--- src/main/java/com/github/avelur/main/Cliente.java
+++ src/main/java/com/github/avelur/main/Cliente.java
@@ -7,14 +7,48 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Cliente {
 	private final static int MAX_BYTES = 1400;
 	private final static String COD_TEXTO = "UTF-8";
 	private final static Scanner SCANNER = new Scanner(System.in);
+    static HashMap<String, String> cuadricula = new HashMap<>();
+    static ArrayList<String> barquitos = new ArrayList<>();
+    static ArrayList<String> barquitos2 = new ArrayList<>();
 	
 	public static void main(String[] args) {
+		System.out.print("\u25fc");
+    	System.out.println("\u25fb");
+    	barquitos.add("A3");barquitos.add("B3");barquitos.add("C3");barquitos.add("D3");
+    	barquitos.add("A6");barquitos.add("A7");barquitos.add("A8");
+    	barquitos.add("F1");barquitos.add("F2");barquitos.add("F3");
+    	barquitos.add("C8");barquitos.add("C9");
+    	barquitos.add("I5");barquitos.add("J5");
+    	barquitos.add("G9");barquitos.add("H9");
+    	barquitos.add("E8");
+    	barquitos.add("G6");
+    	barquitos.add("D5");
+    	barquitos.add("B1");
+    	
+    	barquitos2.add("I4");barquitos2.add("I5");barquitos2.add("I6");barquitos2.add("I7");
+    	barquitos2.add("H10");barquitos2.add("I10");barquitos2.add("J10");
+    	barquitos2.add("B3");barquitos2.add("B4");barquitos2.add("B5");
+    	barquitos2.add("G3");barquitos2.add("H3");
+    	barquitos2.add("F7");barquitos2.add("F8");
+    	barquitos2.add("A10");barquitos2.add("B10");
+    	barquitos2.add("C8");
+    	barquitos2.add("H1");
+    	barquitos2.add("D2");
+    	barquitos2.add("A1");
+    	
+    	if(Integer.parseInt(args[2]) == 1) {
+    		printCuadricula(barquitos);
+    	} else printCuadricula(barquitos2);
+    	
 		String nomHost = args[0];
 		int numPuerto = Integer.parseInt(args[1]);
 		System.out.print("Por favor, introducete: ");
@@ -30,8 +64,20 @@ public class Cliente {
 			while((lineaLeida += brStdIn.readLine()) != null
 					&& lineaLeida.length() > 0) {
 				byte[] datosRecibidos = new byte[MAX_BYTES];
+				
+				String cuadricula = "";
 				InetAddress IPServidor = InetAddress.getByName(nomHost);
-				byte[] b = lineaLeida.getBytes(COD_TEXTO);
+				if(Integer.parseInt(args[2]) == 1) {
+					for(String s: barquitos) {
+						cuadricula += s + ",";
+					}
+				} else {
+					for(String s: barquitos2) {
+						cuadricula += s + ",";
+					}
+				}
+				
+				byte[] b = cuadricula.getBytes(COD_TEXTO);
 				DatagramPacket paqueteEnviado = new DatagramPacket(
 						b, b.length, IPServidor, numPuerto);
 				clientSocket.connect(IPServidor, numPuerto);
@@ -55,5 +101,35 @@ public class Cliente {
 			System.out.println("Excepcion de E/S");
 			ex.printStackTrace();
 		}
+	}
+	
+	public static void printCuadricula(ArrayList<String> barquitas) {
+		String[] abc = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+		System.out.println("Tu cuadricula: ");
+		for(int i = 0; i < 10; i++) {
+    		for(int j = 0; j < 10; j++) {
+    			String quad = abc[i]+(j+1);
+    			if(barquitas.contains(quad)) {
+    				cuadricula.put(abc[i]+(j+1), "\u25fc");
+    			} else cuadricula.put(abc[i]+(j+1), "\u25fb");
+    		}
+    	}
+    	
+    	for(int i = 0; i < 10; i++) {
+    		if(i == 0) {
+    			System.out.print("  ");
+    			for(int k = 0; k < 10; k++) {
+        	    	System.out.print(k+1 + "|");
+            	}
+    			System.out.println();
+    		}
+    		System.out.print(abc[i].toLowerCase() + " ");
+    		
+    		for(int j = 0; j < 10; j++) {
+    			String quad = abc[i]+(j+1);
+    			System.out.print(cuadricula.get(quad) + " ");
+        	}
+    		System.out.println();
+    	}
 	}
 }
--- src/main/java/com/github/avelur/main/Servidor.java
+++ src/main/java/com/github/avelur/main/Servidor.java
@@ -4,83 +4,91 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Servidor extends Thread{
-	private final static int MAX_BYTES = 1400;
-    private final static String COD_TEXTO = "UTF-8";
-    private static int usuarios = 2;
+	private static int MAX_BYTES = 1400;
     private static int numPuerto;
-    private static Thread[] conexiones = new Thread[usuarios];
-    static HashMap<String, String> cuadricula = new HashMap<>();
-    static HashSet<String> barquitas = new HashSet<String>();
     
     public static void main(String[] args) throws IOException, InterruptedException {
-    	System.out.print("\u25fc");
-    	System.out.println("\u25fb");
-    	barquitas.add("A3");barquitas.add("B3");barquitas.add("C3");barquitas.add("D3");
-    	barquitas.add("A6");barquitas.add("A7");barquitas.add("A8");
-    	barquitas.add("F1");barquitas.add("F2");barquitas.add("F3");
-    	barquitas.add("C8");barquitas.add("C9");
-    	barquitas.add("I5");barquitas.add("J5");
-    	barquitas.add("G9");barquitas.add("H9");
-    	barquitas.add("E8");
-    	barquitas.add("G6");
-    	barquitas.add("D5");
-    	barquitas.add("B1");
-    	
-    	printCuadricula(barquitas);
-    	
-		numPuerto = Integer.parseInt(args[0]);
-		for (int i = 0; i < usuarios; i++) {
-			conexiones[i] = new Thread();
-			conexiones[i].start();
+    	numPuerto = Integer.parseInt(args[0]);
+    	byte[] datosRecibidos = new byte[MAX_BYTES];
+    	try (DatagramSocket serverSocket = new DatagramSocket(numPuerto)) {
+    		while(true) {
+    			DatagramPacket paqueteRecibido = new DatagramPacket(
+		             datosRecibidos, datosRecibidos.length);
+    			serverSocket.receive(paqueteRecibido);
+    		
+    			Thread th1 = new Thread(new ServerRecibida(serverSocket, MAX_BYTES, paqueteRecibido));
+    			th1.start();
+    		}
+    		
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
+}
 
+class ServerRecibida implements Runnable{
+	private static String COD_TEXTO = "UTF-8";
+	private DatagramSocket serverSocket;
+	static int MAX_BYTES;
+	static DatagramPacket paqueteRecibido;
+    static HashMap<String, String> cuadricula = new HashMap<String, String>();
+    static HashSet<String> barquitas = new HashSet<String>();
+	
+	public ServerRecibida(DatagramSocket serverSocket, int MAX_BYTES, DatagramPacket paqueteRecibido) {
+		super();
+		this.serverSocket = serverSocket;
+		ServerRecibida.MAX_BYTES = MAX_BYTES;
+		ServerRecibida.paqueteRecibido = paqueteRecibido;
+	}
+	
 	public void run() {
-    	try (DatagramSocket serverSocket = new DatagramSocket(numPuerto)) {
-			while (true) {
-				 byte[] datosRecibidos = new byte[MAX_BYTES];
+		try {
+			System.out.println(1);
+				byte[] datosRecibidos = new byte[MAX_BYTES];
 				 
-			     DatagramPacket paqueteRecibido = new DatagramPacket(
-			             datosRecibidos, datosRecibidos.length);
-			     serverSocket.receive(paqueteRecibido);
-			     InetAddress IPCliente = paqueteRecibido.getAddress();
+			    InetAddress IPCliente = paqueteRecibido.getAddress();
+			    System.out.println(2);
+			    DatagramPacket packetEnviado = 
+			   		 new DatagramPacket(datosRecibidos, datosRecibidos.length,
+			   				 IPCliente, paqueteRecibido.getPort());
+			    //serverSocket.connect(IPCliente, paqueteRecibido.getPort());
+			    System.out.println(3);
+			    //serverSocket.send(packetEnviado);
+			    String lineaRecibida = new String(paqueteRecibido.getData(),
+			            0, paqueteRecibido.getLength(), COD_TEXTO);
+			    System.out.println(4);
+			    String[] cuadriculaString = new String(paqueteRecibido.getData(),
+			            0, paqueteRecibido.getLength(), COD_TEXTO).split(",");
+			    for(int i = 0; i < cuadriculaString.length; i++) {
+			    	barquitas.add(cuadriculaString[i]);
+			    }
+			    System.out.println(5);
+			    printCuadricula(barquitas, IPCliente);
 			     
-			     DatagramPacket packetEnviado = 
-			    		 new DatagramPacket(datosRecibidos, datosRecibidos.length,
-			    				 IPCliente, paqueteRecibido.getPort());
-			     serverSocket.connect(IPCliente, paqueteRecibido.getPort());
-			     serverSocket.send(packetEnviado);
-			     
-			     String lineaRecibida = new String(paqueteRecibido.getData(),
-			             0, paqueteRecibido.getLength(), COD_TEXTO);
-			     
-			     int puertoCliente = paqueteRecibido.getPort();
-			     System.out.printf("Recibido datagrama de %s:%d (%s)\n",
+			    int puertoCliente = paqueteRecibido.getPort();
+			    System.out.printf("Recibido datagrama de %s:%d (%s)\n",
 	                        IPCliente.getHostAddress(), puertoCliente, lineaRecibida);
 			     
-			     String respuesta = "#" + lineaRecibida + "#";
-			     byte[] b = respuesta.getBytes(COD_TEXTO);
-			     DatagramPacket paqueteEnviado = new DatagramPacket(
-			     		b, b.length, IPCliente, puertoCliente);
-			     serverSocket.send(paqueteEnviado);
-			}
+			    String respuesta = "#" + lineaRecibida + "#";
+			    byte[] b = respuesta.getBytes(COD_TEXTO);
+			    DatagramPacket paqueteEnviado = new DatagramPacket(
+			    		b, b.length, IPCliente, puertoCliente);
+			    serverSocket.send(paqueteEnviado);
+			    barquitas.clear();
+			//while (true) {}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
-	public static void printCuadricula(HashSet<String> barquitas) {
+	public static void printCuadricula(HashSet<String> barquitas, InetAddress IPCliente) {
 		String[] abc = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
-		System.out.println("Tu cuadricula: ");
+		System.out.println("Cuadricula de: " + IPCliente.getHostAddress() + ":" + IPCliente.getAddress());
 		for(int i = 0; i < 10; i++) {
     		for(int j = 0; j < 10; j++) {
     			String quad = abc[i]+(j+1);
@@ -107,4 +115,94 @@ public class Servidor extends Thread{
     		System.out.println();
     	}
 	}
+	
+}
+
+
+class ServerRespuesta implements Runnable{
+	private static String COD_TEXTO = "UTF-8";
+	private DatagramSocket serverSocket;
+	private static int MAX_BYTES;
+	private static DatagramPacket paqueteRecibido;
+    static HashMap<String, String> cuadricula = new HashMap<String, String>();
+    static HashSet<String> barquitas = new HashSet<String>();
+	
+	public ServerRespuesta(DatagramSocket serverSocket, int MAX_BYTES, DatagramPacket paqueteRecibido) {
+		super();
+		this.serverSocket = serverSocket;
+		ServerRecibida.MAX_BYTES = MAX_BYTES;
+		ServerRecibida.paqueteRecibido = paqueteRecibido;
+	}
+	
+	public void run() {
+		try {
+			System.out.println(1);
+				byte[] datosRecibidos = new byte[MAX_BYTES];
+				 
+			    InetAddress IPCliente = paqueteRecibido.getAddress();
+			    System.out.println(2);
+			    DatagramPacket packetEnviado = 
+			   		 new DatagramPacket(datosRecibidos, datosRecibidos.length,
+			   				 IPCliente, paqueteRecibido.getPort());
+			    //serverSocket.connect(IPCliente, paqueteRecibido.getPort());
+			    System.out.println(3);
+			    //serverSocket.send(packetEnviado);
+			    String lineaRecibida = new String(paqueteRecibido.getData(),
+			            0, paqueteRecibido.getLength(), COD_TEXTO);
+			    System.out.println(4);
+			    String[] cuadriculaString = new String(paqueteRecibido.getData(),
+			            0, paqueteRecibido.getLength(), COD_TEXTO).split(",");
+			    for(int i = 0; i < cuadriculaString.length; i++) {
+			    	barquitas.add(cuadriculaString[i]);
+			    }
+			    System.out.println(5);
+			    printCuadricula(barquitas, IPCliente);
+			     
+			    int puertoCliente = paqueteRecibido.getPort();
+			    System.out.printf("Recibido datagrama de %s:%d (%s)\n",
+	                        IPCliente.getHostAddress(), puertoCliente, lineaRecibida);
+			     
+			    String respuesta = "#" + lineaRecibida + "#";
+			    byte[] b = respuesta.getBytes(COD_TEXTO);
+			    DatagramPacket paqueteEnviado = new DatagramPacket(
+			    		b, b.length, IPCliente, puertoCliente);
+			    serverSocket.send(paqueteEnviado);
+			    barquitas.clear();
+			//while (true) {}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void printCuadricula(HashSet<String> barquitas, InetAddress IPCliente) {
+		String[] abc = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+		System.out.println("Cuadricula de: " + IPCliente.getHostAddress() + ":" + IPCliente.getAddress());
+		for(int i = 0; i < 10; i++) {
+    		for(int j = 0; j < 10; j++) {
+    			String quad = abc[i]+(j+1);
+    			if(barquitas.contains(quad)) {
+    				cuadricula.put(abc[i]+(j+1), "\u25fc");
+    			} else cuadricula.put(abc[i]+(j+1), "\u25fb");
+    		}
+    	}
+    	
+    	for(int i = 0; i < 10; i++) {
+    		if(i == 0) {
+    			System.out.print("  ");
+    			for(int k = 0; k < 10; k++) {
+        	    	System.out.print(k+1 + "|");
+            	}
+    			System.out.println();
+    		}
+    		System.out.print(abc[i].toLowerCase() + " ");
+    		
+    		for(int j = 0; j < 10; j++) {
+    			String quad = abc[i]+(j+1);
+    			System.out.print(cuadricula.get(quad) + " ");
+        	}
+    		System.out.println();
+    	}
+	}
+	
 }
